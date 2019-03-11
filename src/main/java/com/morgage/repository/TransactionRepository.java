9b2675cc5a6839a0b2c38a6845c5e77c42be00af
Gx <huygia1997@gmail.com>
2019-03-11 20:04:27 +0700

    finish search shop

--- src/main/java/com/morgage/repository/TransactionRepository.java
+++ src/main/java/com/morgage/repository/TransactionRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     List<Transaction> findAllByNextPaymentDateBetween(Date start, Date end);
+
+    List<Transaction> findAllByShopId(int shopId);
 }
--- src/main/java/com/morgage/service/TransactionService.java
+++ src/main/java/com/morgage/service/TransactionService.java
@@ -1,7 +1,27 @@
 package com.morgage.service;
 
+import com.morgage.model.Transaction;
+import com.morgage.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> findTransByShopId(int shopId) {
+        List<Transaction> transactionList = transactionRepository.findAllByShopId(shopId);
+
+        return transactionList;
+
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
 }
